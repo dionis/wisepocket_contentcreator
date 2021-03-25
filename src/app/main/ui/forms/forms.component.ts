ae6fbe2cfefbc800f414e6ffa7b5ac0828aee363
escobar-9912 <david.escobarpozo99@gmail.com>
2021-03-25 20:02:51 +0000

    internacionalizacion prueba1

--- src/app/main/ui/forms/forms.component.ts
+++ src/app/main/ui/forms/forms.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
+import { FileValidator } from 'ngx-material-file-input';
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+
+import { locale as english } from 'app/main/ui/forms/i18n/en';
+import { locale as spanish } from 'app/main/ui/forms/i18n/es';
 
 @Component({
     selector   : 'forms',
@@ -15,19 +20,37 @@ export class FormsComponent implements OnInit, OnDestroy
     horizontalStepperStep1: FormGroup;
     horizontalStepperStep2: FormGroup;
     horizontalStepperStep3: FormGroup;
+    horizontalStepperStep4: FormGroup;
+    readonly maxSize = 104857600;
+    campaignicon:string = 'campaignicon';
+    @ViewChild('fileDorp',{static:false}) inputFile:ElementRef;
 
     // Private
     private _unsubscribeAll: Subject<any>;
+    files: File[] = [];
+    image1: File = undefined;
+    image2: File = undefined;
+    image3: File = undefined;
+    image4: File = undefined;
+    campIconf:File = undefined;
+
+
+
 
     /**
      * Constructor
      *
      * @param {FormBuilder} _formBuilder
+     * @param {FuseTranslationLoaderService} _fuseTranslationLoaderService
      */
     constructor(
-        private _formBuilder: FormBuilder
+        private _formBuilder: FormBuilder,
+        private _fuseTranslationLoaderService: FuseTranslationLoaderService
     )
     {
+        // Load the translations
+        this._fuseTranslationLoaderService.loadTranslations(english, spanish);
+
         // Set the private defaults
         this._unsubscribeAll = new Subject();
     }
@@ -43,20 +66,58 @@ export class FormsComponent implements OnInit, OnDestroy
     {
         // Horizontal Stepper form steps
         this.horizontalStepperStep1 = this._formBuilder.group({
-            firstName: ['', Validators.required],
-            lastName : ['', Validators.required]
+            nameCampaign: ['', [Validators.required, Validators.minLength(100), Validators.maxLength(120)]],
+            description : ['', [Validators.required, Validators.maxLength(800)]]
         });
 
+        const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
         this.horizontalStepperStep2 = this._formBuilder.group({
-            address: ['', Validators.required]
+            phone: ['', [Validators.required,Validators.pattern("^[0-9]*$"),Validators.maxLength(20)]],
+            postalCode: ['', [Validators.required, Validators.maxLength(5)]],
+            website: ['', [Validators.required, Validators.pattern(reg)]],
+            email: ['', [Validators.required, Validators.email, Validators.maxLength(30)]]
         });
 
         this.horizontalStepperStep3 = this._formBuilder.group({
+            country   : ['', Validators.required],
             city      : ['', Validators.required],
-            state     : ['', Validators.required],
-            postalCode: ['', [Validators.required, Validators.maxLength(5)]]
+            state     : ['', Validators.required]
         });
+        console.log(this.inputFile);
+        console.log(this.horizontalStepperStep2.get('phone'));
+        console.log(this.horizontalStepperStep2.get('website'));
+        console.log(this.horizontalStepperStep2.get('email'));
     }
+
+    onChage(event){
+        console.log(event);
+        console.log(this.inputFile);
+    }
+    onSelectIcon(event){
+        console.log(event.addedFiles)
+       this.campIconf = event.addedFiles[0];
+    }
+    onSelect(event) {
+        console.log(event);
+        switch (event.source.id) {
+            case "image1":
+                this.image1 = event.addedFiles[0];
+                break;
+            case "image2":
+                this.image2 = event.addedFiles[0];
+                break;
+            case "image3":
+                this.image3 = event.addedFiles[0];
+                break;
+            case "image4":
+                this.image4 = event.addedFiles[0];
+                break;
+            default:
+                break;
+        }
+        //this.files.push(...event.addedFiles);
+        //console.log(this.files);
+      }
 
     /**
      * On destroy
---
+++ src/app/main/ui/forms/i18n/es.ts
@@ -0,0 +1,31 @@
+export const locale = {
+    lang: 'es',
+    data: {
+        FORMS: {
+          CREATE_CAMPAIGN                  : 'Crear Campaña',
+          CAMPAIGN                         : 'Campaña',
+          NAME_CAMPAIGN_PLACEHOLDER        : 'Nombre de la Campaña',
+          DESCRIPTION_PLACEHOLDER          : 'Descripción',
+          DATA_CAMPAIGN                    : 'Añadir datos de la Campaña',
+		      PHONE_NUMBER_PLACEHOLDER         : 'Número de teléfono',
+          POSTAL_CODE_PLACEHOLDER          : 'Código Postal',
+          WEEBSITE_PLACEHOLDER             : 'Sitio Web',
+          EMAIL_PLACEHOLDER                : 'Correo',
+          CAMPAIGN_REGION                  : 'Region',
+          COUNTRY_PLACEHOLDER              : 'País',
+          CITY_PLACEHOLDER                 : 'Ciudad',
+          STATE_PLACEHOLDER                : 'Estado',
+          LOGO_IMAGES_CAMPAIGN             : 'Añadir logo y imagénes a la Campaña',
+		      CAMPAIGN_ICON_PLACEHOLDER        : 'Ícono de Campaña',
+		      IMAGE1_PLACEHOLDER               : 'Imagen 1',
+		      IMAGE2_PLACEHOLDER               : 'Imagen 2',
+		      IMAGE3_PLACEHOLDER               : 'Imagen 3',
+		      IMAGE4_PLACEHOLDER               : 'Imagen 4',
+          DONE                             : 'Hecho',
+          THANK                            : 'Gracias por completar nuestro formulario',
+          NEXT                             : 'Siguiente',
+          PREVIOUS                         : 'Anterior',
+          SAVE                             : 'Guardar'
+        }
+    }
+};

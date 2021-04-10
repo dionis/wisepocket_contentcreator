d0c34df5b1345c7384bb700045b0d1e5a08d760c
dionis <d.lopez@alltech.fr>
2021-04-09 20:10:00 -0400

    Add editor support

--- assets/app/app/main/apps/contacts/contacts.module.ts
+++ assets/app/app/main/apps/contacts/contacts.module.ts
@@ -19,6 +19,7 @@ import { MatListModule } from '@angular/material/list';
 
 import { FuseSharedModule } from '../../../../@fuse/shared.module';
 import { FuseConfirmDialogModule, FuseSidebarModule } from '../../../../@fuse/components';
+import { FuseWidgetModule } from '@fuse/components/widget/widget.module';
 
 import { ContactsComponent } from '../../../../app/main/apps/contacts/contacts.component';
 import { ContactsService } from '../../../../app/main/apps/contacts/contacts.service';
@@ -27,6 +28,9 @@ import { ContactsSelectedBarComponent } from '../../../../app/main/apps/contacts
 import { ContactsMainSidebarComponent } from '../../../../app/main/apps/contacts/sidebars/main/main.component';
 import { ContactsContactFormDialogComponent } from '../../../../app/main/apps/contacts/contact-form/contact-form.component';
 import { TopicItemComponent } from './contact-list/topic-item/topic-item.component';
+
+import { QuillModule } from 'ngx-quill';
+
 
 const routes: Routes = [
     {
@@ -67,7 +71,24 @@ const routes: Routes = [
 
         FuseSharedModule,
         FuseConfirmDialogModule,
-        FuseSidebarModule
+        FuseSidebarModule,
+        FuseWidgetModule,
+
+        QuillModule.forRoot({
+          modules: {
+            syntax: false,
+            toolbar: [
+              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+              [{ 'font': [] }],
+              ['bold', 'italic', 'underline'],        // toggled buttons
+              [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+              [{ 'align': ''}, { 'align': 'center'}, {'align': 'right'}, {'align': 'justify'}],                      // text direction
+              [{ 'color': [] }, { 'background': [] }],        // dropdown with defaults from theme
+              ['link', 'image', 'video']                         // link and image, video
+            ]
+          },
+          placeholder :"Insert text"
+        }),
     ],
     providers      : [
         ContactsService

48bf7874a5d59e35c05ad2d40692fddb8911a278
Rolando <rolaxel.villaresrey@gmail.com>
2021-04-13 17:54:23 -0400

    First Step of Maps UI whith Leaflet

--- assets/app/app/main/sample/sample.component.ts
+++ assets/app/app/main/sample/sample.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 import { FuseTranslationLoaderService } from '../../../@fuse/services/translation-loader.service';
 
 import { locale as english } from './i18n/en';
 import { locale as turkish } from './i18n/tr';
 import { locale as spanish } from './i18n/es';
+import * as L from 'leaflet' 
 
 @Component({
     selector   : 'sample',
     templateUrl: './sample.component.html',
     styleUrls  : ['./sample.component.scss']
 })
-export class SampleComponent
+export class SampleComponent implements OnInit, AfterViewInit
 {
+    private map;
     /**
      * Constructor
      *
@@ -23,5 +25,26 @@ export class SampleComponent
     )
     {
         this._fuseTranslationLoaderService.loadTranslations(english, turkish, spanish);
+    }
+
+    private initMap(){
+        this.map = L.map('map',{
+            center: [39.8282, -98.5795],
+            zoom: 3
+        });
+        const tiles = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3']
+            //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          });
+      
+        tiles.addTo(this.map);
+    }
+    ngOnInit(){
+
+    }
+
+    ngAfterViewInit(){
+        this.initMap();
     }
 }

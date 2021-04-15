f0a82b05f3f0e63f496d8588e1f7cff8c61acced
Rolando <rolaxel.villaresrey@gmail.com>
2021-04-14 23:51:31 -0400

    Maps V3

--- assets/app/app/main/sample/sample.component.ts
+++ assets/app/app/main/sample/sample.component.ts
@@ -15,6 +15,13 @@ import * as L from 'leaflet'
 export class SampleComponent implements OnInit, AfterViewInit
 {
     private map;
+    private  uris = [
+        'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        'http://localhost:8080/geoserver/wisepocket/wms'
+    ]
+
+
+    serverMap: any;
     /**
      * Constructor
      *
@@ -25,33 +32,53 @@ export class SampleComponent implements OnInit, AfterViewInit
     )
     {
         this._fuseTranslationLoaderService.loadTranslations(english, turkish, spanish);
+        this.serverMap = 'geoServer';
     }
 
     private initMap(){
+        let tiles;
         let corner1 = L.latLng(19.8351702,-84.9514723);
         let corner2 = L.latLng(23.2431588,-74.1343019);
-        const bonus = L.latLngBounds(corner1, corner2);
+        let bonus = L.latLngBounds(corner1, corner2);
+        
+        switch (this.serverMap) { 
+            case 'googleMap':
+                let x,y,z;
+                //x = 18; y = 29; z= 6
+                console.log(this.uris[0])
+                corner1 = L.latLng(19.8351702,-84.9514723);
+                corner2 = L.latLng(23.2431588,-74.1343019);
+                tiles = L.tileLayer(`http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}`,{
+                maxZoom: 20,
+                subdomains:['mt0','mt1','mt2','mt3']
+                //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+              });
+              break;
+            case 'geoServer':
+                corner1 = L.latLng(19.8351702,-84.9514723);
+                corner2 = L.latLng(23.2431588,-74.1343019);
+                bonus = L.latLngBounds(corner1, corner2);
+                tiles = L.tileLayer.wms(this.uris[1],{
+                    layers: 'wisepocket:gis_osm_roads_free_1',
+                    srs: "EPSG:4326",
+                    height: 768,
+                    width:330,
+                    attribution: "CUBA"
+                  });          
+                break;
+            default:
+                break;
+        }
         this.map = L.map('map',{
             center: corner1,
             maxBounds: bonus,
-            crs: L.CRS.EPSG4326,
+            //crs: L.CRS.EPSG4326,
             zoom: 6
         });
-        const tiles = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-            maxZoom: 20,
-            subdomains:['mt0','mt1','mt2','mt3']
-            //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          });
-        
-        const tiles2 = L.tileLayer.wms('http://localhost:8080/geoserver/wisepocket/wms',{
-            layers: 'wisepocket:gis_osm_roads_free_1',
-            height: 768,
-            width:330,
-            attribution: "CUBA"
-          });
-          console.log(tiles2);
-          tiles.addTo(this.map);
-          console.log(this.map)
+        console.log(tiles)
+        tiles.addTo(this.map);
+       
+        //console.log(mauseCoordinates)
     }
     ngOnInit(){
 
@@ -59,5 +86,6 @@ export class SampleComponent implements OnInit, AfterViewInit
 
     ngAfterViewInit(){
         this.initMap();
+        L.control.mousePosition().addTo(this.map);
     }
 }

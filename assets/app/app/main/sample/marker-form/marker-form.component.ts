d045e8a55c32eba73e0f74030b64502e28fc6b32
Rolando <rolaxel.villaresrey@gmail.com>
2021-04-27 11:03:30 -0400

    Commit 27-04-2021 No Marker Images Displayed

--- assets/app/app/main/sample/marker-form/marker-form.component.ts
+++ assets/app/app/main/sample/marker-form/marker-form.component.ts
@@ -90,8 +90,8 @@ export class MarkerContactFormDialogComponent
                 let ext = element.titulo.split('.')[1];
                 await this.imageService.getImage(element).subscribe(response=>{
                     console.log(response);
-                    //this.images.push(response)
-                    this.imgSrcBase = `data:image/png;base64,${response}`
+                    this.images.push(new File([response], element.titulo,{type:'image/'+ext}))
+                    //this.imgSrcBase = `data:image/png;base64,${response}`
                 })
                 //let img = this.imageService.getImage(element);
                 //this.images.push(img);
